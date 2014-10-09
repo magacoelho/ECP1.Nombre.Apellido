6478e9705bc4435654ea96b0128aab5f02483f16
magacoelho <mgacoelho@yahoo.es>
2014-10-09 21:54:16 +0200

    Inicial Nombres y Apellidos

---
+++ src/main/java/es/upm/miw/ECP1/Nombre/Apellido/App.java
@@ -0,0 +1,13 @@
+package es.upm.miw.ECP1.Nombre.Apellido;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
